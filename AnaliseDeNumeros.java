a11edc40886cf763def8a9062bc9fb194d4dfa95
sabrinagobel <b.r.i.m.sg@gmail.com>
2021-07-05 17:48:52 -0300

    Commit inicial

---
+++ AnaliseDeNumeros.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class AnaliseDeNumeros {
+
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+
+        int pares = 0;
+        int impares = 0;
+        int positivos = 0;
+        int negativos = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int a = leitor.nextInt();
+            if (a%2 != 0) impares++;
+            else pares ++;
+            if (a>0) positivos ++;
+            if (a<0) negativos ++;
+        }
+
+        System.out.println(pares + " valor(es) par(es)");
+        System.out.println(impares + " valor(es) impar(es)");
+        System.out.println(positivos + " valor(es) positivo(s)");
+        System.out.println(negativos + " valor(es) negativo(s)");
+    }
+}
--- Idades.java
+++ Idades.java
@@ -20,25 +20,3 @@ public class Idades {
     }
 
 }
-/*
-import java.io.IOException;
-        import java.util.Scanner;
-
-public class  Desafio {
-
-    public static void main(String[] args) throws IOException {
-        Scanner leitor = new Scanner(System.in);
-        int cont = 0;
-        //complete o codigo
-        while (n >= 1) {
-            n = leitor.nextInt();
-            if (n >= 0) {
-                //complete o codigo
-            }
-        }
-        double media = soma / cont;
-        System.out.println(String.format("%.2f", media));
-    }
-
-}*/
-
