53f3fb5f7552af96599509f8af3832d08b453bba
sabrinagobel <b.r.i.m.sg@gmail.com>
2021-07-07 16:18:05 -0300

    novos desafios concluidos

---
+++ QuantidadeDeNumerosPositivos.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class QuantidadeDeNumerosPositivos {
+
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int cont = 0;
+        int quant = 0;
+        while (cont < 6 ) {
+            float x = leitor.nextFloat();
+            if ( x > 0) quant++;
+            cont++;
+        }
+        System.out.println(quant + " valores positivos");
+    }
+}
+
+// Crie um programa que leia 6 valores.
+// Você poderá receber valores negativos e/ou positivos como entrada, devendo desconsiderar os valores nulos.
+// Em seguida, apresente a quantidade de valores positivos digitados.
